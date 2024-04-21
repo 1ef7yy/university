8c2526a125a6f2d7b73fcbf71e453a4bf336aa41
leftyy <ustyantsev.makar@mail.ru>
2024-04-22 01:04:43 +0300

    module excel

---
+++ parser/excel.py
@@ -0,0 +1,10 @@
+from openpyxl import Workbook
+
+def write_to_excel(items):
+    wb = Workbook()
+    ws = wb.active
+    ws.append(list(items[0].keys()))
+    for item in items:
+        ws.append(list(item.values()))
+
+    wb.save("parser/wildberries.xlsx")
--- parser/main.py
+++ parser/main.py
@@ -1,28 +1,14 @@
 # https://github.com/1ef7yy/university/tree/main/parser
 
 from parser import WBParser
-from openpyxl import Workbook
+from excel import write_to_excel
 
 
-def get_cards_data():
-    pen_parser = WBParser("https://www.wildberries.ru/catalog/0/search.aspx?search=%D1%80%D1%83%D1%87%D0%BA%D0%B8%20%D0%B3%D0%B5%D0%BB%D0%B5%D0%B2%D1%8B%D0%B5")
-
-    cards = pen_parser.get_cards(pages_count=5)
-
-    return cards
-
-
-def write_to_excel(items):
-    wb = Workbook()
-    ws = wb.active
-    ws.append(list(items[0].keys()))
-    for item in items:
-        ws.append(list(item.values()))
-
-    wb.save("parser/wildberries.xlsx")
 
 
 if __name__ == "__main__":
-    items = get_cards_data()
-    write_to_excel(items)
+    pen_parser = WBParser("https://www.wildberries.ru/catalog/0/search.aspx?search=%D1%80%D1%83%D1%87%D0%BA%D0%B8%20%D0%B3%D0%B5%D0%BB%D0%B5%D0%B2%D1%8B%D0%B5")
+
+    pens = pen_parser.get_cards(pages_count=5)
+    write_to_excel(pens)
     print("Данные сохранены в wildberries.xlsx!")
